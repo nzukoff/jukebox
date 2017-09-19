918a2afd8a9ee49d90be2f68ac239207c154eca0
Nathan Zukoff <nzukoff@gmail.com>
2017-09-18 19:01:14 -0700

    relates albums and songs

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,4 +2,23 @@ class AlbumsController < ApplicationController
     def index
         @albums = Album.all
     end
+
+    def new
+        @album = Album.new
+    end
+
+    def show
+        @album = Album.find(params[:id])
+    end
+
+    def create
+        name = params[:album][:name]
+        Album.create({name: name})
+        redirect_to albums_path
+    end
+
+    def destroy
+        Album.find(params[:id]).destroy
+        redirect_to albums_path
+    end
 end
